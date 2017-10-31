037f01bdd3082dd1579bd41d2937f205ec96e1b8
GaryNg <garyng.zhongbo@gmail.com>
2017-10-31 22:23:01 +0800

    build: add start and dev scripts

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Setup the PATH environment variable to include the hugo binary included with this repo
-set PATH=%PATH%;%~dp0bin\
-cmd
+set PATH=%PATH%;%~dp0bin\;%~dp0scripts\
+cmd /k cd site
